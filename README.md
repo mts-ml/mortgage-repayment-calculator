30a7e438d40357b6cc0ead7205068320d97dc155
Mateus <mtsmlima@gmail.com>
2025-02-03 18:59:46 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,66 @@
+# Frontend Mentor - Mortgage repayment calculator solution
+
+This is a solution to the [Mortgage repayment calculator challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/mortgage-repayment-calculator-Galx1LXK73). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Links](#links)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- Input mortgage information and see monthly repayment and total repayment amounts after submitting the form
+- See form validation messages if any field is incomplete
+- Complete the form only using their keyboard
+- View the optimal layout for the interface depending on their device's screen size
+- See hover and focus states for all interactive elements on the page
+
+### Links
+
+- [Preview Site](https://your-live-site-url.com)
+
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- [React](https://reactjs.org/) - JS library
+- [Styled Components](https://styled-components.com/) - For styles
+
+### What I learned
+
+Use this section to recap over some of your major learnings while working through this project. Writing these out and providing code samples of areas you want to highlight is a great way to reinforce your own knowledge.
+
+To see how you can add code snippets, see below:
+
+```html
+<h1>Some HTML code I'm proud of</h1>
+```
+```css
+.proud-of-this-css {
+  color: papayawhip;
+}
+```
+```js
+const proudOfThisFunc = () => {
+  console.log('🎉')
+}
+```
+
+If you want more help with writing markdown, we'd recommend checking out [The Markdown Guide](https://www.markdownguide.org/) to learn more.
+
+## Author
+
+- Frontend Mentor - [@mts-ml](https://www.frontendmentor.io/profile/mts-ml)
---
+++ src/components/Results/Results.jsx
@@ -0,0 +1,66 @@
+import emptyImg from '../../assets/images/illustration-empty.svg'
+import PropTypes from 'prop-types'
+
+import './resultsStyle.scss'
+
+
+export default function Results(props) {
+   Results.propTypes = {
+      formValues: PropTypes.object.isRequired,
+      isSubmit: PropTypes.bool.isRequired,
+      formErrors: PropTypes.object.isRequired,
+      monthlyPayment: PropTypes.number.isRequired,
+      totalPayment: PropTypes.number.isRequired,
+   }
+
+   function formatCurrency(value) {
+      if (value === null) {
+         return "£0.00"
+      }
+      
+      return value.toLocaleString('en-GB',
+         {
+            style: 'currency', currency: "GBP"
+         }
+      )
+   }
+
+
+
+   return (
+      <aside>
+         {Object.keys(props.formErrors).length === 0 && props.isSubmit ? (
+            <div className="aside__full">
+               <h2 className="aside__title left">Your results</h2>
+
+               <p className='aside__description left'>
+                  Your results are shown below based on the information you provided.
+                  To adjust the results, edit the form and click “calculate repayments” again.
+               </p>
+
+               <div className="aside__money">
+                  <p className="aside__info">Your monthly repayments</p>
+
+                  <span className="aside__payment-month">{formatCurrency(props.monthlyPayment)}</span>
+
+                  <hr className='aside__hr' />
+
+                  <p className="aside__info">Total you will repay over the term</p>
+
+                  <span className="aside__payment-total">{formatCurrency(props.totalPayment)}</span>
+               </div>
+            </div>
+         ) : (
+            <div className="aside__empty">
+               <img className='aside__img' src={emptyImg} alt="Image of a calculator and money" />
+
+               <h2 className="aside__title">Results shown here</h2>
+
+               <p className='aside__description'>
+                  Complete the form and click “calculate repayments” to see what your monthly repayments would be.
+               </p>
+            </div>
+         )}
+      </aside>
+   )
+}
