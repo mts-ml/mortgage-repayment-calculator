8322ad5cd21685b97a7436fb20acfe1ba881c912
Mateus <mtsmlima@gmail.com>
2025-02-26 19:29:16 -0300

    Changed extension to .ts

--- src/main.jsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import App from './App'
-
-
-// The '!' tells TypeScript that 'document.getElementById('root')' will never be null.
-createRoot(document.getElementById('root')).render(
-  <App />
-)
