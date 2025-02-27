f512556e0fc7f333d086d81ab8e5392f5954afc0
Mateus <mtsmlima@gmail.com>
2025-02-27 11:24:39 -0300

    Changed file extension to .ts

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,6 +1,7 @@
 import js from '@eslint/js'
 import globals from 'globals'
 import react from 'eslint-plugin-react'
+// @ts-ignore
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 
